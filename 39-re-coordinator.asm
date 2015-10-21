d567f54d4adcb2956a5785db1042dea1f3854b17
Austin <gamepin126@gmail.com>
2015-10-21 00:53:39 -0700

    add 39

---
+++ 39-re-coordinator.asm
@@ -0,0 +1,7 @@
+/*Each number in the inbox is an aaddress of a tile on the floor.
+Send to the outbox the coordinates of that tile, column first, row second.*/
+
+-- HUMAN RESOURCE MACHINE PROGRAM --
+
+main:
+  
