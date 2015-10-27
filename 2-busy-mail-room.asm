beaa346d3fd241c5ddc47e88ec300873f6d5f868
Austin <gamepin126@gmail.com>
2015-10-27 00:49:11 -0700

    turns out human resource machine really likes CRLF :(

--- 2-busy-mail-room.asm
+++
@@ -1,8 +0,0 @@
-/*Grab each thing from the inbox, and drop each one into the outbox.*/
-
--- HUMAN RESOURCE MACHINE PROGRAM --
-
-a:
-    INBOX
-    OUTBOX
-    JUMP     a
--- 39-re-coordinator.asm
+++
@@ -1,7 +0,0 @@
-/*Each number in the inbox is an aaddress of a tile on the floor.
-Send to the outbox the coordinates of that tile, column first, row second.*/
-
--- HUMAN RESOURCE MACHINE PROGRAM --
-
-main:
-  
